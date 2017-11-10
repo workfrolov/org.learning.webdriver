8950e1dc796360d8ca0995f379fa8470100dde4f
workfrolov <work.frolov@gmail.com>
2017-11-10 02:28:13 +0200

    test

--- src/main/java/CodeToTest/TestingFeature.java
+++ src/main/java/CodeToTest/TestingFeature.java
@@ -1,27 +1,3 @@
 package CodeToTest;
 
-import net.bytebuddy.asm.Advice;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import static CodeToTest.TestingFeature.searchResult;
-
-public class TestingFeature {
-    //public static void main(String[] args) throws InterruptedException {
-    //    searchResult();
-    //}
-    public static void searchResult(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\chromedriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        String startUrl = "http://www.google.com";
-        driver.get(startUrl);
-        String checkUrl = driver.getCurrentUrl();
-        WebElement searchform = driver.findElement(By.id("searchform"));
-        searchform.sendKeys("webdriver tutorial");
-        searchform.sendKeys(Keys.ENTER);
-
-    }
-}
--- src/test/java/TestSuit/Tests.java
+++ src/test/java/TestSuit/Tests.java
@@ -1,16 +1,50 @@
 package TestSuit;
 
-import CodeToTest.TestingFeature;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
 
-
-
+import java.util.concurrent.TimeUnit;
 
 public class Tests {
-@Test
-    public static void Test1(){
-    Assert.assertEquals(driver.get(startUrl), TestingFeature.checkUrl);
+    private static WebDriver driver;
+
+    @BeforeTest
+    public static void setDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void gRequest() {
+        driver.get("https://www.google.com");
+        Assert.assertEquals("https://www.google.com",driver.getCurrentUrl());
+        WebElement searchform = driver.findElement(By.id("lst-ib"));
+        String searchRequest = "webdriver tutorial";
+        searchform.sendKeys(searchRequest);
+        searchform.sendKeys(Keys.ENTER);
+        String searchResult = driver.findElement(By.id("lst-ib")).getAttribute("value");
+        Assert.assertEquals(searchResult,searchRequest );
+    }
+
+    @Test
+    public void rc(){
+        driver.get("https://www.ringcentral.com");
+
+    }
+
+    @AfterTest
+    public static void closeDriver() {
+        driver.quit();
+    }
 }
-}
+
 

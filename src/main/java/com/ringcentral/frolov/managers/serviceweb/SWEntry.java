493cada67ca94685482f28443505755c37ceae61
Alexander Zaverukha <alexander.zaverukha@ab-soft.net>
2017-12-10 12:31:39 +0200

    remove code

--- src/main/java/com/ringcentral/frolov/managers/serviceweb/SWEntry.java
+++
@@ -1,8 +0,0 @@
-package com.ringcentral.frolov.managers.serviceweb;
-
-/**
- * Created by alexanderzaverukha on 11/19/17.
- */
-public interface SWEntry {
-    public static final String AMS = "https://service-amsup.lab.nordigy.ru";
-}
